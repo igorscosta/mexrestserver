8746a9a49ab38c065f272dc6ad6118068fdeccb2
Igor Costa <igorsc93@gmail.com>
2016-04-13 08:31:25 -0300

    Adding POST method to consume a JSON with the author name from any client.

--- src/main/java/org/akws/mex/MexController.java
+++ src/main/java/org/akws/mex/MexController.java
@@ -3,7 +3,26 @@ package org.akws.mex;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
-import javax.ws.rs.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.util.Iterator;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.Response;
+import javax.print.attribute.standard.Media;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created by igorcosta on 12/04/16.
@@ -14,17 +33,28 @@ public class MexController {
     @Path("/getauthorname")
     @GET
     @Produces("application/json")
-    public JsonArray getAuthorName(){
+    public JsonArray getAuthorName() {
         JsonArrayBuilder builder = Json.createArrayBuilder();
-        builder.add(Json.createObjectBuilder().add("Athor", "Igu Costa"));
+        builder.add(Json.createObjectBuilder().add("Author", "Igu Costa"));
         return builder.build();
     }
 
-    /*@Path("/setauthorname")
+    @Path("/setauthorname")
     @POST
     @Consumes("application/json")
-    public JsonArray setAuthorName(){
-        String authorName = json.
-    }*/
+    public String setAuthorName(String content) throws ParseException {
+        JSONParser parser = new JSONParser();
+        String stringToParse = content;
+        Object obj = parser.parse(stringToParse);
+        JSONObject jsonObject = (JSONObject) obj;
+        String authorName = (String) jsonObject.get("Author");
+        System.out.println("Nome do autor:" + authorName);
+        //return Response.status(201);
+        return "Author: " + authorName;
 
+    }
 }
+
+
+
+
